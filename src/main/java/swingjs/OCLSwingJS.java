cd06377180853249741599e8d26435dacf466458
bobhanson <hansonr@stolaf.edu>
2024-12-20 00:15:10 -0600

    CDX/CDXML refactoring, adds bracket [..]n functionality

--- src/main/java/swingjs/OCLSwingJS.java
+++ src/main/java/swingjs/OCLSwingJS.java
@@ -5,18 +5,16 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
 
 import com.actelion.research.chem.AbstractDepictor;
 import com.actelion.research.chem.MolfileCreator;
-import com.actelion.research.chem.MolfileV3Creator;
 import com.actelion.research.chem.SmilesParser;
 import com.actelion.research.chem.StereoMolecule;
 import com.actelion.research.chem.moreparsers.CDXParser;
@@ -33,6 +31,59 @@ public class OCLSwingJS {
 
 	public static void main(String[] args) {
 		showDialogTest(null);
+		System.exit(0);
+	}
+
+	@SuppressWarnings("unused")
+	private static void showDialogTest(String[] args) {
+
+		// Molecule.setDefaultAverageBondLength(HiDPIHelper.scale(12));
+
+		new FileHelper(null).readStructuresFromFileAsync(false, (list) -> {
+			StereoMolecule mol;
+			String title;
+			if (list == null || list.isEmpty()) {
+				String smiles = JOptionPane.showInputDialog("enter a SMILES",
+						"" + "N12C(=O)OC(C)(C)C.C1CC[C@H]2C(=O)[NH]C1=CC=CC2=CC=CC=C12"
+//			+ "CC(CCCC(C1CCC2C1(C)CCC1C2CC=C2C1(C)CCC(C2)O)C)C"
+//						+ "CCN.[b]12-c3ccccc3.o1[b]4-c5ccccc5.o4[b]6-c7ccccc7.o62"
+				);
+				title = smiles;
+				mol = new SmilesParser().parseMolecule(smiles); // Nc1cc(OCCO)cc(N)c1
+			} else {
+				title = "mol";
+				mol = (StereoMolecule) list.get(0);
+			}
+
+			showEditFrame(mol);
+			// test for SVG output
+//
+//			SVGDepictor svgd = new SVGDepictor(mol, mode, "");
+//			svgd.validateView(null, new GenericRectangle(0, 0, 300, 200), AbstractDepictor.cModeInflateToHighResAVBL);
+//			svgd.paint(null);
+//			svgd.setLegacyMode(false);
+//			System.out.println(svgd.toString());
+//
+		});
+
+	}
+
+	private static void showEditFrame(StereoMolecule mol) {
+		SwingEditorPanel p = new SwingEditorPanel(mol);
+		p.setSize(new Dimension(500, 500));
+
+		int mode = AbstractDepictor.cDModeSuppressCIPParity | AbstractDepictor.cDModeSuppressESR
+				| AbstractDepictor.cDModeSuppressChiralText;
+
+		SwingEditorArea area = (SwingEditorArea) p.getComponent(0);
+		area.getGenericDrawArea().setDisplayMode(mode);
+
+		SwingDialog d = new SwingDialog(null, "testing2");
+		
+		d.setSize(new Dimension(500, 500));
+		d.add(p);
+		d.setVisible(true);
+		d.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 	}
 
 	private static void testSmilesParser(String outdir) {
@@ -198,57 +249,6 @@ public class OCLSwingJS {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	@SuppressWarnings("unused")
-	private static void showDialogTest(String[] args) {
-
-		// Molecule.setDefaultAverageBondLength(HiDPIHelper.scale(12));
-
-		new FileHelper(null).readStructuresFromFileAsync(false, (list) -> {
-			StereoMolecule mol;
-			String title;
-			if (list == null || list.isEmpty()) {
-				String smiles = JOptionPane.showInputDialog("enter a SMILES",
-						"" + "N12C(=O)OC(C)(C)C.C1CC[C@H]2C(=O)[NH]C1=CC=CC2=CC=CC=C12"
-//			+ "CC(CCCC(C1CCC2C1(C)CCC1C2CC=C2C1(C)CCC(C2)O)C)C"
-//						+ "CCN.[b]12-c3ccccc3.o1[b]4-c5ccccc5.o4[b]6-c7ccccc7.o62"
-				);
-				title = smiles;
-				mol = new SmilesParser().parseMolecule(smiles); // Nc1cc(OCCO)cc(N)c1
-			} else {
-				title = "mol";
-				mol = (StereoMolecule) list.get(0);
-			}
-
-			showEditFrame(mol);
-			// test for SVG output
-//
-//			SVGDepictor svgd = new SVGDepictor(mol, mode, "");
-//			svgd.validateView(null, new GenericRectangle(0, 0, 300, 200), AbstractDepictor.cModeInflateToHighResAVBL);
-//			svgd.paint(null);
-//			svgd.setLegacyMode(false);
-//			System.out.println(svgd.toString());
-//
-		});
-
-	}
-
-	private static void showEditFrame(StereoMolecule mol) {
-		SwingEditorPanel p = new SwingEditorPanel(mol);
-		p.setSize(new Dimension(500, 500));
-
-		int mode = AbstractDepictor.cDModeSuppressCIPParity | AbstractDepictor.cDModeSuppressESR
-				| AbstractDepictor.cDModeSuppressChiralText;
-
-		SwingEditorArea area = (SwingEditorArea) p.getComponent(0);
-		area.getGenericDrawArea().setDisplayMode(mode);
-
-		SwingDialog d = new SwingDialog(null, "testing2");
-
-		d.setSize(new Dimension(500, 500));
-		d.add(p);
-		d.setVisible(true);
 	}
 
 }
