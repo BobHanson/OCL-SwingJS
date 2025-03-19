2350471d8009d75450d4a473b85d75a5a23a0e72
bobhanson <hansonr@stolaf.edu>
2025-03-18 23:09:21 -0500

    delocalization update from OCL main

--- src/main/java/com/actelion/research/chem/AromaticityResolver.java
+++ src/main/java/com/actelion/research/chem/AromaticityResolver.java
@@ -95,7 +95,9 @@ public class AromaticityResolver {
 
 		if (mayChangeAtomCharges)
 			for (int atom=0; atom<mMol.getAtoms(); atom++)
-				if (mIsDelocalizedAtom[atom] && mMol.getAtomicNo(atom) == 7 && mMol.getConnAtoms(atom) == 3)
+				if (mIsDelocalizedAtom[atom] && mMol.getAtomicNo(atom) == 7
+				 && (mIsDelocalizedBridgeHead[atom]
+				  || (mMol.getConnAtoms(atom) == 3 && !mIsDelocalizedFiveRingMember[atom])))
 					mMol.setAtomCharge(atom, 1);
 
 		mAllHydrogensAreExplicit = allHydrogensAreExplicit;
@@ -450,6 +452,7 @@ public class AromaticityResolver {
                         int terminalBond = bond;
                         int bridgeAtom = mMol.getBondAtom(1-i, bond);
                         while (terminalBond != -1) {
+							mIsDelocalizedAtom[terminalAtom] = false;
                             mIsDelocalizedBond[terminalBond] = false;
                             mDelocalizedBonds--;
                             mMol.setBondType(terminalBond, Molecule.cBondTypeDelocalized);
@@ -462,13 +465,15 @@ public class AromaticityResolver {
                                         bridgeAtom = mMol.getConnAtom(terminalAtom, j);
                                         }
                                     else {
-                                        terminalAtom = -1;
+                                        terminalAtom = -1;	// Stop here! We have hit an aromatic branch.
                                         terminalBond = -1;
                                         break;
                                         }
                                     }
                                 }
                             }
+						if (terminalAtom != -1)		// Regular end of aromatic chain (no branch).
+							mIsDelocalizedAtom[bridgeAtom] = false;
                         break;
                         }
                     }
